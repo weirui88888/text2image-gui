094bedae38d88832cfdbf43561f9278f42065a39
weirui88888 <ruiwei88888@163.com>
2023-10-23 11:40:44 +0800

    feat(env.local): use .env.local to set process.env

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -51,8 +51,8 @@ function SignUp() {
   }
 
   initAliyunCaptcha({
-    SceneId: 'pcflzxg9', // 场景ID。根据步骤二新建验证场景后，您可以在验证码场景列表，获取该场景的场景ID
-    prefix: 'wr8jiu', // 身份标。开通阿里云验证码2.0后，您可以在控制台概览页面的实例基本信息卡片区域，获取身份标
+    SceneId: process.env.REACT_APP_ALI_CAPTCHA_SCENE_ID, // 场景ID。根据步骤二新建验证场景后，您可以在验证码场景列表，获取该场景的场景ID
+    prefix: process.env.REACT_APP_ALI_CAPTCHA_PREFIX, // 身份标。开通阿里云验证码2.0后，您可以在控制台概览页面的实例基本信息卡片区域，获取身份标
     mode: 'popup', // 验证码模式。popup表示要集成的验证码模式为弹出式。无需修改
     element: '#sign-up-captcha-element', // 页面上预留的渲染验证码的元素，与原代码中预留的页面元素保持一致。
     button: '#sign-up-submit-button', // 触发验证码弹窗的元素。button表示单击登录按钮后，触发captchaVerifyCallback函数。您可以根据实际使用的元素修改element的值
