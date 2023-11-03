4ed969acb09f21fb670f0c35f030340393625a6c
weirui88888 <ruiwei88888@163.com>
2023-11-03 16:27:48 +0800

    feat(congratulation): successful do deploy-web and deploy-server

--- server/controller/photo.js
+++ server/controller/photo.js
@@ -49,7 +49,7 @@ const generatePhoto = async (req, res) => {
   }
 }
 const deletePhoto = async (req, res) => {
-  res.send(`photo delete route,port:${process.env.PORT}`)
+  res.send(`photo delete route,you start port at :${process.env.PORT}`)
 }
 
 module.exports = {
