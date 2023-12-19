4a357558008076a408b3f57bfc28ac9386ecd224
weirui88888 <ruiwei88888@163.com>
2023-12-19 15:30:48 +0800

    feat(keybord): support press enter keyboard to generate image

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,20 @@
 import React, { useRef, useEffect, useState } from 'react'
-import { Textarea, Tooltip } from '@geist-ui/core'
+import { Textarea, Tooltip, useKeyboard, KeyCode, Keyboard } from '@geist-ui/core'
 import autosize from 'autosize'
-import { PenTool } from '@geist-ui/icons'
+import { PenTool, X } from '@geist-ui/icons'
 import ButtonRound from '@/components/ButtonRound'
 import './index.css'
 
+const mockGenerate = () => {
+  return new Promise((res, rej) => {
+    setTimeout(res, 3000)
+  })
+}
+
 const Home = () => {
   const textareaRef = useRef(null)
+  const [generate, setGenerate] = useState(false)
+  const [value, setValue] = useState<string>('')
   useEffect(() => {
     const textareaDom = textareaRef.current
     autosize(textareaDom!)
@@ -15,14 +23,28 @@ const Home = () => {
     }
   }, [])
 
-  const [value, setValue] = useState<string>('')
   const handler = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setValue(e.target.value)
-    console.log(e.target.value)
   }
+  const generateImage = async () => {
+    setGenerate(true)
+    await mockGenerate()
+    setGenerate(false)
+  }
+
+  const { bindings } = useKeyboard(
+    () => {
+      if (!generate && value) {
+        generateImage()
+      }
+    },
+    [KeyCode.Enter],
+    { disableGlobalEvent: true }
+  )
   return (
     <div className="home-page">
       <Textarea
+        {...bindings}
         width="100%"
         placeholder="type any thing you like..."
         rows={16}
@@ -32,27 +54,47 @@ const Home = () => {
         value={value}
         onChange={handler}
       ></Textarea>
+      {value && (
+        <Tooltip
+          style={{ position: 'absolute', bottom: '60px', right: '10px' }}
+          text="clear input..."
+          placement="left"
+          scale={0.5}
+        >
+          <ButtonRound
+            auto
+            icon={<X />}
+            onClick={() => {
+              ;(textareaRef.current as any).style.removeProperty('height')
+              setValue('')
+            }}
+          />
+        </Tooltip>
+      )}
+
       {!value ? (
-        <>
-          <Tooltip
-            scale={0.5}
-            text="type something before you click this button..."
-            style={{ position: 'absolute', bottom: '10px', right: '10px' }}
-            placement="left"
-          >
-            <ButtonRound disabled auto aria-label="Glossary" icon={<PenTool />} />
-          </Tooltip>
-        </>
-      ) : (
-        <ButtonRound
-          auto
-          aria-label="Glossary"
+        <Tooltip
+          scale={0.5}
+          text="😊 type something before you click this button..."
           style={{ position: 'absolute', bottom: '10px', right: '10px' }}
-          icon={<PenTool />}
-          onClick={() => {
-            console.log('docs')
-          }}
-        />
+          placement="left"
+        >
+          <ButtonRound disabled auto icon={<PenTool />} />
+        </Tooltip>
+      ) : (
+        <Tooltip
+          scale={0.5}
+          text={
+            <>
+              click this button or press {''}
+              <Keyboard scale={0.5}>Enter</Keyboard>
+            </>
+          }
+          style={{ position: 'absolute', bottom: '10px', right: '10px' }}
+          placement="left"
+        >
+          <ButtonRound loading={generate} auto icon={<PenTool />} onClick={generateImage} />
+        </Tooltip>
       )}
     </div>
   )
